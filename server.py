0acc71cc0a47fcddf4471af2560e101c5dc7ab11
lazyjul <45462701+lazyjul@users.noreply.github.com>
2019-04-04 17:11:45 +0200

    de spaghetti

--- server.py
+++ server.py
@@ -4,13 +4,43 @@ import json
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(20)
 
 clientList = []
+
+class backendCommands:
+    def __init__(self):
+        pass
+
+    def message(self, messageData, conn, clientData):
+        bname = bytes(str(clientData["clientNick"]), "utf-8")
+        data = bname + b": " + bytes(" ".join(messageData), "utf-8")
+        sendAll(data)
+        return True
+
+    def end(self, messageData, conn, clientData):
+        global clientList
+        data = b'end'
+        conn.send(data)  # echo
+        conn.close()
+        clientList.remove(conn)
+        bname = bytes(str(clientData["clientNick"]), "utf-8")
+        sendAll(bname + b" disconnected")
+        return False
+
+    def changeName(self, messageData, conn, clientData):
+        bname = bytes(str(clientData["clientNick"]), "utf-8")
+        bdata = bytes(" ".join(messageData[0:]), "utf-8")
+        data = bname + b" changed name to " + bdata
+        sendAll(data)
+        clientData["clientNick"] = conn.recv(BUFFER_SIZE).decode("utf-8")
+        return True
+
+BackendCommands = backendCommands()
 
 def sendAll(data):
     global clientList
@@ -21,34 +51,21 @@ def sendAll(data):
             clientList.remove(i)
 
 def clientHandler(conn, clientData):
-    global clientList
     #message handler
-    while True:
+    run = True
+    while run:
         data = conn.recv(BUFFER_SIZE)
         if not data: break
-        printdata=data.decode("utf-8")
-        print("received data:", clientData["clientNick"], printdata)
-        if "sys" in printdata:
-            printdata = printdata[4:]
-            if printdata == "end":
-                data = b'end'
-                conn.send(data)  # echo
-                conn.close()
-                clientList.remove(conn)
-                print("dropped:", clientData["clientNick"])
-                bname = bytes(str(clientData["clientNick"]), "utf-8")
-                sendAll(bname + b" disconnected")
-                break
-            elif "changed name to" in printdata: 
-                bname = bytes(str(clientData["clientNick"]), "utf-8")
-                bdata = bytes(printdata, "utf-8")
-                data = bname + b" " + bdata
-                sendAll(data)
-                clientData["clientNick"] = conn.recv(BUFFER_SIZE).decode("utf-8")
-        else:   
-            bname = bytes(str(clientData["clientNick"]), "utf-8")
-            data = bname + b": " + data
-            sendAll(data)
+        messageData=data.decode("utf-8")
+        print("received data:", clientData["clientNick"], messageData)
+        if "sys" in messageData:
+            messageData = messageData[4:]
+            messageData = messageData.split()
+            run = getattr(BackendCommands, messageData[0])(messageData[1:], conn, clientData)
+        else:
+            pass
+    print("dropped:", clientData["clientNick"])
+            
 
 while True:
     conn, addr = s.accept()
